8e7c333ba8d9586737b03bfa790351b98ab3bccd
daleyjones <djones_15@hotmail.com>
2023-06-25 16:52:02 +0100

    notes updated

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 
-// Import our modular routers for /tips and /feedback
+
 const notesRouter = require('./notes');
-const feedbackRouter = require('./notes');
-// Import the diagnostics route
-const diagnosticsRouter = require('./diagnostics');
+
+;
 
 const app = express();
 
 app.use('/api/notes', notesRouter);
-app.use('/feedback', feedbackRouter);
-// Initialize diagnostics route
-app.use('/diagnostics', diagnosticsRouter);
+
 
 module.exports = app;
--- routes/notes.js
+++ routes/notes.js
@@ -1,27 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const notesFilePath = path.join(__dirname, 'notes.json');
 
 
-let notes = [
-  { id: 1, title: 'Note 1', content: 'This is note 1' },
-  { id: 2, title: 'Note 2', content: 'This is note 2' },
-];
+function readNotes() {
+  try {
+    const notesData = fs.readFileSync(notesFilePath, 'utf8');
+    return JSON.parse(notesData);
+  } catch (error) {
+    console.error('Error reading notes file:', error);
+    return [];
+  }
+}
 
+
+function writeNotes(notes) {
+  try {
+    fs.writeFileSync(notesFilePath, JSON.stringify(notes), 'utf8');
+  } catch (error) {
+    console.error('Error writing notes file:', error);
+  }
+}
 
 router.get('/', (req, res) => {
+  const notes = readNotes();
   res.json(notes);
 });
 
 router.post('/', (req, res) => {
   const { title, content } = req.body;
-  const newNote = { id: Date.now(), title, content };
+  const newNote = { id: Date.now().toString(), title, content };
+  const notes = readNotes();
   notes.push(newNote);
+  writeNotes(notes);
   res.status(201).json(newNote);
 });
 
-
 router.get('/:id', (req, res) => {
-  const noteId = parseInt(req.params.id);
+  const noteId = req.params.id;
+  const notes = readNotes();
   const note = notes.find((note) => note.id === noteId);
   if (note) {
     res.json(note);
@@ -30,12 +50,13 @@ router.get('/:id', (req, res) => {
   }
 });
 
-
 router.delete('/:id', (req, res) => {
-  const noteId = parseInt(req.params.id);
+  const noteId = req.params.id;
+  let notes = readNotes();
   const index = notes.findIndex((note) => note.id === noteId);
   if (index !== -1) {
     const deletedNote = notes.splice(index, 1)[0];
+    writeNotes(notes);
     res.json(deletedNote);
   } else {
     res.status(404).json({ message: 'Note not found' });
