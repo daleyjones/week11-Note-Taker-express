d58886ca07eaebe18f3cbbedce9818c4805d83b0
daleyjones <djones_15@hotmail.com>
2023-06-25 17:22:47 +0100

    notes changed

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
-const notesFilePath = path.join(__dirname, '..', 'db', 'notes.json');
+const notesFilePath = path.join(__dirname, '../db/notes.json');
 
 
 function readNotes() {
