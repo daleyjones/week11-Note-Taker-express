25441aa42b1e06bfaa8f616ce78de58a79bc847d
daleyjones <djones_15@hotmail.com>
2023-06-25 14:36:08 +0100

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
+const notesRouter = require('./notes');
 const feedbackRouter = require('./feedback');
 // Import the diagnostics route
 const diagnosticsRouter = require('./diagnostics');
 
 const app = express();
 
-app.use('/tips', tipsRouter);
+app.use('/api/notes', notesRouter);
 app.use('/feedback', feedbackRouter);
 // Initialize diagnostics route
 app.use('/diagnostics', diagnosticsRouter);
