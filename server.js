01771abcc3e064ab65006a7f50e25a3c72336447
daleyjones <djones_15@hotmail.com>
2023-06-25 16:03:54 +0100

    notes update

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) => {
+// GET Route for notes page
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
